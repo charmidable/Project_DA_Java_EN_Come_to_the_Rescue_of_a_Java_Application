f24179443372d580cc6ae1384291aba2e65d1a44
geget2705 <geget2705@gmail.com>
2022-03-07 09:50:31 +0100

    package for the Main class

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/main/Main.java
@@ -0,0 +1,12 @@
+package com.hemebiotech.analytics.main;
+
+public class Main
+{
+    public class Main
+    {
+        public static void main(String args[])
+        {
+            new FileTextToFileTextSymptomsAnalyser("symptoms.txt", "result.out").makeAndSaveSymptomAnalyse();
+        }
+    }
+}
